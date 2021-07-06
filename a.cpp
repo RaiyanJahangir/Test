6ab96ef2e2b3e07878be748169964ac5c06a2873
Raiyan <raiyan.jahangir@gmail.com>
2021-07-06 20:45:19 +0600

    while loop added

--- a.cpp
+++ a.cpp
@@ -3,24 +3,29 @@ using namespace std;
 int main()
 {
     int a;
-    cout<<"Enter an option: 1. Add 2. Multiply"<<endl;
-    cin>>a;
-    if(a==1)
+    while(1)
     {
-        int b,c;
-        cout<<"Enter first number:";
-        cin>>b;
-        cout<<"Enter second number:";
-        cin>>c;
-        cout<<"Sum is "<<b+c<<endl;
-    }
-    else if(a==2)
-    {
-        int b,c;
-        cout<<"Enter first number:";
-        cin>>b;
-        cout<<"Enter second number:";
-        cin>>c;
-        cout<<"Product is "<<b*c<<endl;
+        cout<<"Enter an option: 1. Add 2. Multiply"<<endl;
+        cin>>a;
+        if(a==1)
+        {
+            int b,c;
+            cout<<"Enter first number:";
+            cin>>b;
+            cout<<"Enter second number:";
+            cin>>c;
+            cout<<"Sum is "<<b+c<<endl;
+            break;
+        }
+        else if(a==2)
+        {
+            int b,c;
+            cout<<"Enter first number:";
+            cin>>b;
+            cout<<"Enter second number:";
+            cin>>c;
+            cout<<"Product is "<<b*c<<endl;
+            break;
+        }
     }
 }
