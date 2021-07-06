6d8c44e5d504f086c433b79f97284a7740dccfad
Raiyan <raiyan.jahangir@gmail.com>
2021-07-06 20:54:17 +0600

    subtraction added

--- a.cpp
+++ a.cpp
@@ -5,7 +5,7 @@ int main()
     int a;
     while(1)
     {
-        cout<<"Enter an option: 1. Add 2. Multiply"<<endl;
+        cout<<"Enter an option: 1. Add 2. Multiply 3.Subtract"<<endl;
         cin>>a;
         if(a==1)
         {
@@ -25,6 +25,16 @@ int main()
             cout<<"Enter second number:";
             cin>>c;
             cout<<"Product is "<<b*c<<endl;
+            break;
+        }
+        else if(a==3)
+        {
+            int b,c;
+            cout<<"Enter first number:";
+            cin>>b;
+            cout<<"Enter second number:";
+            cin>>c;
+            cout<<"Difference is "<<b-c<<endl;
             break;
         }
     }
