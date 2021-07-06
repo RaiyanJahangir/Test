4643d44e700aaa60a5a2873c417b654a9fcda33b
Raiyan <raiyan.jahangir@gmail.com>
2021-07-06 19:48:25 +0600

    b.cpp added

--- a.cpp
+++ a.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 int main()
 {
-
+    int a;
+    cout<<"Enter an option: 1. Add 2. Multiply"<<endl;
+    cin>>a;
+    if(a==1)
+    {
+        int b,c;
+        cout<<"Enter first number:";
+        cin>>b;
+        cout<<"Enter second number:";
+        cin>>c;
+        cout<<"Sum is "<<b+c<<endl;
+    }
 }
