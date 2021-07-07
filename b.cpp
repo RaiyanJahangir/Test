f925319991fa9c916b129eba3774623fa8854603
yeasintaha <meahmeah82@gmail.com>
2021-07-07 17:01:11 +0600

    some other changes

--- b.cpp
+++ b.cpp
@@ -3,4 +3,5 @@ using namespace std;
 int main()
 {
     cout<<"This is a dumb file"<<endl;
+    cout<<"This is just an experiment"<<endl;
 }
---
+++ d.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string i;    
+    cin>>i;
+    cout<<"This is string i "<<i<<endl;
+    return 0;
+}
