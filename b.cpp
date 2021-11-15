4408f75ea37a5a3523e99574d4eaf0e02b5d8773
Raiyan <raiyan.jahangir@gmail.com>
2021-11-15 10:36:39 +0600

    cpp files removed

--- b.cpp
+++
@@ -1,7 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    cout<<"This is a dumb file"<<endl;
-    cout<<"This is just an experiment"<<endl;
-}
--- e.cpp
+++
@@ -1,7 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-    cout<<"Rj vai high cg "<<endl;
-    return 0;
-}
